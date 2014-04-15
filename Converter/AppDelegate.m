843a4d01985bf3a37ed7aae56cb9080ce76121f0
Keith Sharp <keith.sharp@gmail.com>
2014-04-15 16:12:01 +0100

    Fixed linking issues, see blog for details, and cleaned up conversion code.

--- Converter/AppDelegate.m
+++ Converter/AppDelegate.m
@@ -9,11 +9,56 @@
 #import "AppDelegate.h"
 #include "proj_api.h"
 
-// Probably shouldn't have these as globals!
-projPJ pjWGS84;
-projPJ pjOSGB;
+static NSMutableArray *grid = nil;
+
+NSString *gridRefNumToLet(int eastings, int northings, int digits)
+{
+    // Get the 100K grid indices
+    int e100k = floor(eastings/100000);
+    int n100k = floor(northings/100000);
+    if (e100k < 0 || e100k > 6 || n100k < 0 || n100k > 12) {
+        return nil;
+    }
+    
+    // Convert indices to letter pair
+    NSString *letters = [NSString stringWithFormat:@"%@", [[grid objectAtIndex:n100k] objectAtIndex:e100k]];
+    
+    // Strip 100K grid and reduce precision
+    int e = floor((eastings%100000)/pow(10, 5-digits/2));
+    int n = floor((northings%100000)/pow(10, 5-digits/2));
+    
+    // Assemble and return the grid reference
+    return [NSString stringWithFormat:@"%@%d%d", letters, e, n];
+}
+
+@interface AppDelegate ()
+{
+    projPJ pjWGS84;
+    projPJ pjOSGB;
+}
+@end
 
 @implementation AppDelegate
+
++(void)initialize
+{
+    if (!grid) {
+        grid = [[NSMutableArray alloc] initWithCapacity:13];
+        [grid insertObject:@[@"SV", @"SW", @"SX", @"SY", @"SZ", @"TV", @"Tw"] atIndex:0];
+        [grid insertObject:@[@"SQ", @"SR", @"SS", @"ST", @"SU", @"TQ", @"TR"] atIndex:1];
+        [grid insertObject:@[@"SL", @"SM", @"SN", @"SO", @"SP", @"TL", @"TM"] atIndex:2];
+        [grid insertObject:@[@"SF", @"SG", @"SH", @"SJ", @"SK", @"TF", @"TG"] atIndex:3];
+        [grid insertObject:@[@"SA", @"SB", @"SC", @"SD", @"SE", @"TA", @"TB"] atIndex:4];
+        [grid insertObject:@[@"NV", @"NW", @"NX", @"NY", @"NZ", @"OV", @"Ow"] atIndex:5];
+        [grid insertObject:@[@"NQ", @"NR", @"NS", @"NT", @"NU", @"OQ", @"OR"] atIndex:6];
+        [grid insertObject:@[@"NL", @"NM", @"NN", @"NO", @"NP", @"OL", @"OM"] atIndex:7];
+        [grid insertObject:@[@"NF", @"NG", @"NH", @"NJ", @"NK", @"OF", @"OG"] atIndex:8];
+        [grid insertObject:@[@"NA", @"NB", @"NC", @"ND", @"NE", @"OA", @"OB"] atIndex:9];
+        [grid insertObject:@[@"HV", @"HW", @"HX", @"HY", @"HZ", @"JV", @"Jw"] atIndex:10];
+        [grid insertObject:@[@"HQ", @"HR", @"HS", @"HT", @"HU", @"JQ", @"JR"] atIndex:11];
+        [grid insertObject:@[@"HL", @"HM", @"HN", @"HO", @"HP", @"JL", @"JM"] atIndex:12];
+    }
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -36,9 +81,9 @@ projPJ pjOSGB;
     y = DEG_TO_RAD * self.latitude.doubleValue;
  
     if ((e = pj_transform(pjWGS84, pjOSGB, 1, 0, &x, &y, NULL)) != 0) {
-        [self.osgbRef setStringValue:@"Transform Error"];
+        [self.osgbRef setStringValue:@"Error"];
     } else {
-        [self.osgbRef setStringValue:[NSString stringWithFormat:@"%d %d", @(x).intValue, @(y).intValue]];
+        [self.osgbRef setStringValue:gridRefNumToLet((int)floor(x), (int)floor(y), 6)];
     }
 }
 
